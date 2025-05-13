60e0a8a60f42da4c617ca4dc7856b2000870ba22
EmmyAina <ainae07@gmail.com>
2025-05-13 11:04:55 -0400

    fix unused

--- helper/shell.py
+++ helper/shell.py
@@ -5,7 +5,7 @@ def encrypt_file(file_data, password, algo):
 	# I cleaned the data to strip qouations and b string
 	cleaned_data = file_data.strip().replace('"', '\\"')
 	# used ~printf '%s'~ in prace of echo to avoid new line aes-256-cbc
-	command = f"printf '%s' '{cleaned_data}' | openssl enc -aes-256-cbc -out helper/encoded.enc -pbkdf2 -pass pass:{password}"
+	command = f"printf '%s' '{cleaned_data}' | openssl enc -{algo} -out helper/encoded.enc -pbkdf2 -pass pass:{password}"
 
 	subprocess.run(command, shell=True)
 def decrypt_file(file, password, algo):
